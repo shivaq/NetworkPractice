712439c5aaa948657303de8a5775449f5fc21ae8
Yasuaki <shivaq777@gmail.com>
2018-04-20 07:29:18 +0900

    kk

--- Python/udemyPython/例外処理とテスト/simple2.py
+++ Python/udemyPython/例外処理とテスト/simple2.py
@@ -1,16 +1,14 @@
 """
-モジュールドキュメントを入れて。。。
 A very simple script.
 """
 
 def myfunc():
     """
-    関数ドキュメントを入れて。。。
     An extremely simple function.
     """
     first = 1
     second = 2
     print(first)
-    print(second)
+    print('second')
 
 myfunc()
