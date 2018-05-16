8c337038c16b2524efdb94f7725b9c0ee258a39e
Yasuaki <shivaq777@gmail.com>
2018-05-17 07:44:11 +0900

    just to upload

---
+++ Aws/CloudFormation/Advanced/CodysDevelopmentLLC/HOW_TO_RUN.java
@@ -0,0 +1,11 @@
+sharedinfrastructure.yaml
+// 基盤となるインフラを生成する
+
+autosubnet.yaml
+// Lambda で使用可能な CIDR レンジを自動取得する
+
+portalbucketanduser.yaml
+ブラウザ経由でCFnに環境を生成させるための材料が入ったバケット
+
+appenvWithTestFunction.yaml
+アプリのインストールやテストを行う
